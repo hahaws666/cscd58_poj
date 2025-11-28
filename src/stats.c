d402b101266939b85c22a54e20962661bfa50545
Quanjiang Li <130194129+QJLiAndrew@users.noreply.github.com>
2025-11-28 15:16:24 -0500

    function name fix
    1

--- src/stats.c
+++ src/stats.c
@@ -1,5 +1,6 @@
 #include "monitor.h"
 #include <stdio.h>
+#include <math.h> 
 
 void stats_init(ping_stats_t *s) {
     s->total_sent = 0;
@@ -13,24 +14,29 @@ void stats_init(ping_stats_t *s) {
     s->previous_rtt = 0.0;
 }
 
-void statsUpdate(ping_stats_t *s, int success, double rtt) {
+void stats_update_ping(ping_stats_t *s, int success, double rtt) {
     s->total_sent++;
 
     if (success) {
+        s->total_received++;
         if (s->total_received > 1) {
             double diff = rtt - s->previous_rtt;
-            if (diff < 0) diff = -diff;
+            if (diff < 0) diff = -diff; 
             s->jitter_ms += (diff - s->jitter_ms) / 16.0;
         }
-        
         s->previous_rtt = rtt;
-        s->total_received++;
+
         s->last_rtt_ms = rtt;
+
+        // Update min/max
         if (rtt < s->min_rtt_ms) s->min_rtt_ms = rtt;
         if (rtt > s->max_rtt_ms) s->max_rtt_ms = rtt;
+
+        // For average
         s->sum_rtt_ms += rtt;
     }
 
+    // Update loss rate
     s->loss_rate = 1.0 - ((double)s->total_received / (double)s->total_sent);
 }
 
@@ -43,9 +49,10 @@ void stats_print(const ping_stats_t *s) {
     printf("| Loss Rate            | %-14.2f%% |\n", s->loss_rate * 100.0);
     
     if (s->total_received > 0) {
+        double avg = s->sum_rtt_ms / (double)s->total_received;
         printf("| Min RTT              | %-12.2f ms |\n", s->min_rtt_ms);
         printf("| Max RTT              | %-12.2f ms |\n", s->max_rtt_ms);
-        printf("| Avg RTT              | %-12.2f ms |\n", s->sum_rtt_ms / s->total_received);
+        printf("| Avg RTT              | %-12.2f ms |\n", avg);
         printf("| Jitter               | %-12.2f ms |\n", s->jitter_ms);
     }
     printf("+----------------------+-----------------+\n");
